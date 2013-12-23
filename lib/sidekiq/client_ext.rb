2f82b88654f21ac6d1ef73f139544d7d3cf2cb32
MJ Alvarez <mj.alvarez@belldigitalmedia.com.au>
2013-12-23 13:38:04 +0800

    undefined method Class' (NameError)

--- lib/sidekiq/client_ext.rb
+++ lib/sidekiq/client_ext.rb
@@ -2,13 +2,14 @@
 module Sidekiq
   class Client
     class << self
-      alias_method :original_normalize_item, :normalize_item
 
       def normalize_item(item)
         normalized_item = original_normalize_item(item)
         normalized_item['jid'] = item['jid'] if item['jid'].present?
         normalized_item
       end
+
+      alias_method :original_normalize_item, :normalize_item
     end
   end
 end
